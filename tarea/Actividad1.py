9daad2dcc89e5753bf7a5cde4958818d0e00f5ff
IsaacR <isaacrevelo55@gmail.com>
2023-04-24 17:33:31 -0500

    Tarea pa la house y hacer comentarios en los codigos del teacher

---
+++ tarea/Actividad1.py
@@ -0,0 +1,11 @@
+#Calcula el numero mayor de entre 3 numeros
+
+n1=int(input("Escriba un numero entero: "))
+n2=int(input("Escriba un numero entero: "))
+n3=int(input("Escriba un numero entero: "))
+
+if n1>n2:
+    if n1>n3:
+        print("El numero mayor es: ", n1)
+    else:
+        print("El numero mayor es: ", n)
---
+++ tarea/Actividad2.py
@@ -0,0 +1,5 @@
+#El programa calculara la hora despues de un segundo de la hora que se le de
+
+H=int(input("Ingrese la hora en formato 24hrs: "))
+M=int(input("Ingrese la hora en formato 24hrs: "))
+S=int(input("Ingrese la hora en formato 24hrs: "))
