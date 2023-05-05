1b3b639f469645dd820ecff26256a0a8823ea9b7
IsaacR <isaacrevelo55@gmail.com>
2023-05-05 10:13:15 -0500

    Actualizacion lista1

--- Actividad3mayo/lista1.py
+++ Actividad3mayo/lista1.py
@@ -8,6 +8,7 @@ for i in range(tam):
     lista.append(num)
 print (lista)
 
+#PROMEDIO
 for i in range(len(lista)):
     suma+=lista[i]
     total=suma
@@ -15,6 +16,7 @@ for i in range(len(lista)):
 print(f"La suma total de los numeros es {total}")
 print(f"El promedio es {prom}")
 
+#MAYOR Y MENOR
 mayor=0
 menor=1000000
 
@@ -26,10 +28,26 @@ for i in lista:
 print(f"El numero mayor es {mayor}")
 print(f"El numero menor es {menor}")
 
-
+#MODA
 moda=0
-
 for i in lista:
-    if lista[i]==lista[i+1]:
-        moda
+    if tam==i:
+       moda=i 
 print(f"La moda es {moda}")
+
+#MEDIANA
+lista.sort()
+print(lista)
+m=(len(lista))
+if m%2==0:
+    mediana=(lista[m//2-1]+lista[m//2])/2
+else:
+    mediana=lista[m//2]
+print (f"La mediana es {mediana}")
+
+#DESVIACION ESTANDAR
+for i in lista:
+    resta=i-prom
+    cuadrado=resta**2
+    division=cuadrado//suma
+    print(division)
