5a6cab50824c9db025dc598f518551db946f2327
IsaacR <isaacrevelo55@gmail.com>
2023-06-14 11:23:58 -0500

    Actividad clases y herencias

--- Act9Junio/App.py
+++ Act9Junio/App.py
@@ -2,9 +2,18 @@ from Empresa import *
 from Individual import *
 from Producto import *
 
-c1=Empresa("Isy",125367,2243532)
-p1=Producto("Refresco","Limonada")
+c1 = Individual("Isy", 125367, 2243532)
+p1 = Producto("Refresco", "Limonada", 2000)
 c1.agregarprod(p1)
 print(p1.getnompro())
+
 for producto in c1.getprod():
     print(producto.getnompro())
+
+nombre = "Refresco"
+c1.descuentoI(nombre)
+
+for producto in c1.getprod():
+    if producto.getnompro() == nombre:
+        print("Descuento aplicado al producto", producto.getnompro())
+        print("Precio actualizado:", producto.getprecio())
